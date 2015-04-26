f8b3876548efc66bfd799bdbd4686d62a0670357
nkh <navid.khazaee@gmail.com>
2015-04-26 19:09:38 +0430

    Corrected script and converted data

--- 1/Scripts/changeUnknownWithMean.sh
+++ 1/Scripts/changeUnknownWithMean.sh
@@ -4,14 +4,15 @@ rm -rf ./*.col
 
 #change delimeter character to seperate with what you want
 char_delim=","
+char_unknown="?"
 
 for i in `seq $(head -1 "$FILE_NAME" | tr $char_delim ' ' | wc -w | cut -d' ' -f1)`
 do 
     echo "Processing column $i ... "
-    cat "$FILE_NAME" | cut -d$char_delim -f$i | grep -iv '?' > ./tmp_file
+    cat "$FILE_NAME" | cut -d$char_delim -f$i | grep -iv "$char_unknown" > ./tmp_file
     mean=$(printf "%.4f\n" "$(echo "(`cat ./tmp_file | tr '\n' '+0' && echo "0"`)/$(wc -l ./tmp_file | cut -d' ' -f1)" | bc -l )")
     echo "Mean for column $i is $mean ...."
-   cat "$FILE_NAME" | cut -d$char_delim -f$i | sed "s/\?/$mean/g" >> "./tmp_$i.col"
+    cat "$FILE_NAME" | cut -d$char_delim -f$i | sed "s/"$char_unknown"/$mean/g" >> "./tmp_$i.col"
 done
 paste -d $char_delim `ls *.col  | sort -V` > "$FILE_NAME".result
 
