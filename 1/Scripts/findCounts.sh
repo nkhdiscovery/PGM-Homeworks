1debc4f49229ad6d39e9b8b7f35629a0718f01e6
nkh <navid.khazaee@gmail.com>
2015-04-27 15:26:07 +0430

    Bug fix for findCounts.sh in case of unsorted query

--- 1/Scripts/findCounts.sh
+++ 1/Scripts/findCounts.sh
@@ -3,23 +3,27 @@ declare -A columnMap
 export DB=test.db
 
 mkdir joints temp 2>/dev/null
+querySorted=$(echo $1 | tr ',' '\n' | sort -g | tr '\n' ',' | sed 's/,$//g' )
+echo "$querySorted"
+
 rm -rf ./joints/"$1.joint"
+rm -rf ./joints/"$querySorted.joint"
 
 # see which columns are queried to find their joint
 while read a
 do 
     columnMap[$a]="`grep $a ./vars.define | cut -d':' -f2`" ;
     cols="$cols,${columnMap[$a]}"
-done < <(echo $1 | tr ',' '\n') 
+done < <(echo $querySorted | tr ',' '\n' | sort -g) 
 cols=`echo $cols | sed 's/^,//g'`
 
-cat $DB | cut -d, -f$cols > ./temp/"$1.cut"
+cat $DB | cut -d, -f$cols > ./temp/"$querySorted.cut"
 
 total=`wc -l "$DB" | cut -d' ' -f1`
 
 while read jointEvent
 do
-    count=`grep "$jointEvent" ./temp/"$1.cut" | wc -l| cut -d' ' -f1`
-    echo -n "$jointEvent" >> ./joints/"$1.joint"
-    printf " %.9f\n" "$(echo "$count/$total" | bc -l)" >>  ./joints/"$1.joint"
-done < <(./combineVars.sh $1 | tr ' ' '\n' )
+    count=`grep "$jointEvent" ./temp/"$querySorted.cut" | wc -l| cut -d' ' -f1`
+    echo -n "$jointEvent" >> ./joints/"$querySorted.joint"
+    printf " %.9f\n" "$(echo "$count/$total" | bc -l)" >>  ./joints/"$querySorted.joint"
+done < <(./combineVars.sh $querySorted | tr ' ' '\n' )
