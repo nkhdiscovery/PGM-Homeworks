3bbe20b9abf397fd9a088d3d20fec677de135c15
nkh <navid.khazaee@gmail.com>
2015-04-28 11:55:34 +0430

    Now probQuery and createCPD are ready, I think ... .

--- 1/Scripts/createCPD.sh
+++ 1/Scripts/createCPD.sh
@@ -1,5 +1,7 @@
 export queryVar=`echo $1 | cut -d'|' -f1`
 export conditions=`echo "$1" | cut -d'|' -f2 | tr ',' '\n' | sort -g | tr '\n' ',' | sed 's/,$//g'`
+mkdir ./CPD/ 2> /dev/null
+rm -rf ./CPD/"$queryVar-$conditions".cpd
 
 ./makeJointTable.sh "$conditions"
 ./makeJointTable.sh "$queryVar,$conditions" #sorting will be handled there
@@ -15,11 +17,14 @@ do
     pJointAll=`./probQuery.sh "$sortedAll" "$i"`
     conDitionValue=`echo $i | cut -d, -f$qIndex --complement`
     pCondition=`./probQuery.sh "$conditions" "$conDitionValue"`
-    if [[ `echo $pCondition'=='0.0 | bc -l` -eq 1 ]] 
-    then
-        >&2 echo "OOooooPs! P($conDitionValue) is zero!! This should never happen because P($i) should be epsilon!\n Exiting ... Check the program or your run sequences." 
-        exit 0 ;
-    fi
-    echo "$pCondition : $pJointAll"
+
+    # I removed this due to performance, I hope it never happens ! :D I handled enough in probQuery.sh
+#    if [[ `echo $pCondition'=='0.0 | bc -l` -eq 1 ]] 
+#    then
+#        >&2 echo "OOooooPs! P($conDitionValue) is zero!! This should never happen because P($i) should be epsilon!\n Exiting ... Check the program or your run sequences." 
+#        exit 0 ;
+#    fi
+    echo -n "$i " >> ./CPD/"$queryVar-$conditions".cpd
+    echo $pJointAll $pCondition | awk '{printf "%.9f\n", $1/$2 == 0 ? 0.000000001 : $1/$2}' >> ./CPD/"$queryVar-$conditions".cpd
 done
 
--- 1/Scripts/probQuery.sh
+++ 1/Scripts/probQuery.sh
@@ -6,15 +6,18 @@
 p=`grep "$2" ./joints/"$1.joint" | cut -d' ' -f2 `
 if [[ -z "$p" ]]
 then 
-    >&2 echo "Warning, no entry for $2 in $1.joint ... considering epsilon = 0.000001"
-    echo 0.000001
+    >&2 echo "Warning, no entry for $2 in $1.joint ... considering epsilon = 0.000000001"
+    echo 0.000000001
 else
-    if [[ `echo $pCondition'=='0.0 | bc -l` -eq 1 ]] 
-    then
-        >&2 echo "Warning, entry for $2 in $1.joint is zero ... considering epsilon = 0.000001"
-        echo 0.000001
-    else
-        echo "$p"
-    fi
+    echo $p | awk '{printf"%.9f\n" , $0 ==0 ? 0.000000001 : $0}'
+    # I did below with the line above and I don't warn, as the performance goes up with awk rather than if on bc
+#    if [[ `echo $p'=='0.0 | bc -l` -eq 1 ]] 
+#    then
+#        >&2 echo "Warning, entry for $2 in $1.joint is zero ... considering epsilon = 0.000000001"
+#        echo 0.000000001
+#    else
+#        echo "$p"
+#    fi
+
 fi
 
