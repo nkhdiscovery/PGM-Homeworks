373363380d31e183140f26dd0925516ee2aa2e97
nkh <navid.khazaee@gmail.com>
2015-04-28 11:03:11 +0430

    Still developing, forgot to handle zero probability due to not being in dataset

--- 1/Scripts/createCPD.sh
+++ 1/Scripts/createCPD.sh
@@ -4,11 +4,22 @@ export conditions=`echo "$1" | cut -d'|' -f2 | tr ',' '\n' | sort -g | tr '\n' '
 ./makeJointTable.sh "$conditions"
 ./makeJointTable.sh "$queryVar,$conditions" #sorting will be handled there
 sortedAll=`echo $queryVar,$conditions | tr ',' '\n' | sort -g | tr '\n' ',' | sed 's/,$//g' `
-echo $sortedAll
+
+qIndex=`echo $sortedAll | tr ',' ' ' | grep -o ".*$queryVar" | wc -w`
+
 
 queryValues="`cat $queryVar.var | tr ',' '\n' `"
-for i in `./combineVars.sh "$sortedAll"`
+
+for i in `./combineVars.sh $sortedAll`
 do
-    echo $i
+    pJointAll=`./probQuery.sh "$sortedAll" "$i"`
+    conDitionValue=`echo $i | cut -d, -f$qIndex --complement`
+    pCondition=`./probQuery.sh "$conditions" "$conDitionValue"`
+    if [[ `echo $pCondition'=='0.0 | bc -l` -eq 1 ]] 
+    then
+        >&2 echo "OOooooPs! P($conDitionValue) is zero!! This should never happen because P($i) should be epsilon!\n Exiting ... Check the program or your run sequences." 
+        exit 0 ;
+    fi
+    echo "$pCondition : $pJointAll"
 done
 
--- 1/Scripts/probQuery.sh
+++ 1/Scripts/probQuery.sh
@@ -6,9 +6,15 @@
 p=`grep "$2" ./joints/"$1.joint" | cut -d' ' -f2 `
 if [[ -z "$p" ]]
 then 
-    >&2 echo "Warning, no entry for $2 in $1.joint ... considering zero"
-    echo 0.0
+    >&2 echo "Warning, no entry for $2 in $1.joint ... considering epsilon = 0.000001"
+    echo 0.000001
 else
-    echo "$p"
+    if [[ `echo $pCondition'=='0.0 | bc -l` -eq 1 ]] 
+    then
+        >&2 echo "Warning, entry for $2 in $1.joint is zero ... considering epsilon = 0.000001"
+        echo 0.000001
+    else
+        echo "$p"
+    fi
 fi
 
