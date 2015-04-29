156c765b0e4a62dc31ced510e9c5faf4d88438f2
nkh <navid.khazaee@gmail.com>
2015-04-29 22:41:28 +0430

    Cleaning my footstep as debugger which cause this version not to work :D

--- 1/Scripts/makeNthQuery.sh
+++ 1/Scripts/makeNthQuery.sh
@@ -5,7 +5,6 @@ vNum=`wc -l $varsDefine | cut -d' ' -f1`
 vars=`cat $varsDefine | cut -d':' -f1 | tr '\n' ',' | sed 's/,$//g'`
 dbCols=`cat $varsDefine | cut -d':' -f2 | tr '\n' ',' | sed 's/,$//g'`
 line=`sed -n "${lineNum}p" "$DB" | cut -d',' -f$dbCols `
-echo "$line"
 cond="`echo $vars | cut -d, -f$vNum`=`echo $line | cut -d, -f$vNum `"
 evidence="`echo $vars | cut -d, -f$vNum --complement`"
 line=`echo $line | cut -d, -f$vNum --complement | tr ',' '\n' `
